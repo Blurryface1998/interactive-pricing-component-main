d4ad971c4518770fa96b73354f9e24c54c190557
Blurryface1998 <83365791+Blurryface1998@users.noreply.github.com>
2022-04-08 20:23:00 +0200

    Part 3

--- app/js/script.js
+++ app/js/script.js
@@ -1,38 +1,126 @@
-let slider, sliderfills, thumb, slidervalue;
-let initalValue = [8, 12, 16, 24, 36];
+const slider = document.getElementById("slider");
+const selector = document.getElementById("selector");
+const selectorValue = document.getElementById("selectorValue");
+const progressBar = document.getElementById("progressBar");
+const yearlyB = document.getElementById("yearlyB");
+const monthlyB = document.getElementById("monthlyB");
+const views = document.getElementById("views");
+const radioBG = document.getElementById("bg");
+const radioBtn = document.querySelectorAll("input[type=radio]");
+let yearState = true;
 
-document.addEventListener("DOMContentLoaded", (e) => {init();});
 
-function init() {
-    slider = document.querySelector("#range");
-    sliderfills = document.querySelector(".range__background");
-    thumb = document.querySelector(".range__thumb");
-    slidervalue = document.querySelector('value');
+/* Slider Button Position on input */
+slider.addEventListener("input", () => {
+    selector.style.left = slider.value + "%";
+    setPrice();
+}); 
 
+/* Setting Price on Input range moving and Background width change */
+function setPrice() {
+    let value = parseInt(slider.value);
+    switch(value) {
+        case 1:
+            views.innerHTML = "10K pageviews";
+            selectorValue.innerHTML = getPrice(yearState, value, 8);
+            progressBar.style.width = 1 + "%";
+            break;
+        case 2:
+            views.innerHTML = "50K pageviews";
+            selectorValue.innerHTML = getPrice(yearState, value, 12);
+            progressBar.style.width = 25 + "%";
+            break;
+        case 3:
+            views.innerHTML = "100K pageviews";
+            selectorValue.innerHTML = getPrice(yearState, value, 16);
+            progressBar.style.width = 50 + "%";
+            break;
+        case 4:
+            views.innerHTML = "500k pageviews";
+            selectorValue.innerHTML = getPrice(yearState, value, 24);
+            progressBar.style.width = 75 + "%";
+            break;
+        case 5:
+            views.innerHTML = "1M pageviews";
+            selectorValue.innerHTML = getPrice(yearState, value, 36);
+            progressBar.style.width = 100 + "%";
+            break;
+        default:
+            break;  
+    }
+}
+
+function getPrice(state, value, price) {
+    let newPrice;
     
-        slider.addEventListener("input", (e) => {
-            updateSlider(slider.value);
-        });
-        slider.addEventListener("change", (e) => {
-            updateSlider(slider.value);
-        });
-        slider.value = initalValue;
-
-        updateSlider(slider.value);
+    if(state) {
+        switch(value) {
+            case 1:
+                newPrice = "$".concat(price, ".00");
+                break;
+            case 2:
+                newPrice = "$".concat(price, ".00");
+                break;
+            case 3:
+                newPrice = "$".concat(price, ".00");
+                break;
+            case 4:
+                newPrice = "$".concat(price, ".00");
+                break;
+            case 5:
+                newPrice = "$".concat(price, ".00");
+                break;
+            default:
+                break;
+        }
     }
 
-function updateSlider(fill, val) {
-    setThumbText(slidervalue, val);
-    setSliderFill(sliderfills);
+    else if(!state) {
+        switch(value) {
+            case 1:
+                newPrice = getDiscount(price);
+                break;
+            case 2:
+                newPrice = getDiscount(price);
+                break;
+            case 3:
+                newPrice = getDiscount(price);
+                break;
+            case 4:
+                newPrice = getDiscount(price);
+                break;
+            case 5:
+                newPrice = getDiscount(price);
+                break;
+            default:
+                break;
+        }
+    }
+    return newPrice;
 }
 
-function setThumbText(elem, val) {
+/* Getting Discount of 25% when you click on the Yearly Radio Button */
+function getDiscount(price) {
+    let money = price * 0.25;
+    return "$".concat(money, ".00");
+
+}
+
+for(let i = 0; i < radioBtn.length; i++) {
+    radioBtn[i].addEventListener("change", (e) => {
+
+        if(radioBtn[i] == yearlyB) {
+            radioBG.classList.add("active");
+            yearState = false;
+            setPrice();
+        } 
+        
+        else if(radioBG.classList.contains("active") === true) {
+            radioBG.classList.remove("active");
+            yearState = true;
+            setPrice();
+        }
+        e.preventDefault();
+    });
     
-}
-
-
-
-function setSliderFill(elem) {
-    let fillColor = getComputedStyle(elem).getPropertyValue("--strongCyan-SliderBackround");
-    elem.style.BackgroundColor = fillColor;
 }
